bff41ba200159b041f244c96896e885c5246c011
Zac Colley <hi@zaccolley.com>
2013-12-21 22:22:19 +0000

    icon swap JS

--- js/build/script.js
+++ js/build/script.js
@@ -1,5 +1,38 @@
 // ajax goes here lol
 (function(){
-	var a = "lol";
-	console.log(a);
+	iconSwap();
 })()
+
+
+// to swap any text with dirty icons
+function iconSwap(){
+
+	// get list of icons
+	var icons = document.querySelectorAll('.icon-swap');
+	// list of icons to swap out the text with
+	var iconSwapLookup = {
+		'Search': 'search',
+		'Grid': 'th-large',
+		'List': 'th-list',
+		'Checkout': 'shopping-cart'
+	};
+
+	for(var i in icons){		
+		var icon = icons[i];
+
+		// if the object has innerHTML
+		if(icon.hasOwnProperty('innerHTML')){
+
+			// swap the text with the icon name
+			var className = iconSwapLookup[icon.innerHTML];
+			icon.innerHTML = '<i class="fa fa-'+className+'"></i>';
+
+			// remove the class showing it was for swapping
+			icon.classList.remove('icon-swap');
+
+		}
+
+	}
+
+}
+	
