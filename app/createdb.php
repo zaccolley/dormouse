d9e86b8dcb07d00b0982fe19836e495f8a645183
Zac Colley <hi@zaccolley.com>
2014-05-01 00:01:57 +0100

    Removed trash SQL files

--- app/createdb.php
+++ app/createdb.php
@@ -27,9 +27,10 @@
 			INSERT INTO item (item_name, item_desc, item_price, item_stock, item_img, cat_id) VALUES ("Sausage Dog", "Its pretty sick. Its a dog.", 50, 100, true, 1);
 			INSERT INTO item (item_name, item_desc, item_price, item_stock, item_img, cat_id) VALUES ("West Highland Terrier", "This is our familys dog and its really annoying.", 75, 250, true, 1);
 			INSERT INTO item (item_name, item_desc, item_price, item_stock, item_img, cat_id) VALUES ("Ocelot", "Salvidor Dali had one these cuties&hellip;", 7500, 2, true, 2);
-			INSERT INTO item (item_name, item_desc, item_price, item_stock, item_img, cat_id) VALUES ("Pepper", "My cat, way too fluffy", 1000000, 1, false, 2);
-			INSERT INTO item (item_name, item_desc, item_price, item_stock, item_img, cat_id) VALUES ("Adobe Dreamweaver CS6", "Where dreams are weaved!", 9999, 0, false, 3);
-			INSERT INTO item (item_name, item_desc, item_price, item_stock, item_img, cat_id) VALUES ("Inkscape", "Some vector stuff", 20, 0, false, 3);';
+			INSERT INTO item (item_name, item_desc, item_price, item_stock, item_img, cat_id) VALUES ("Pepper", "My cat, way too fluffy", 1000000, 1, true, 2);
+			INSERT INTO item (item_name, item_desc, item_price, item_stock, item_img, cat_id) VALUES ("Adobe Dreamweaver CS6", "Where dreams are weaved!", 9999, 5, true, 3);
+			INSERT INTO item (item_name, item_desc, item_price, item_stock, item_img, cat_id) VALUES ("Inkscape", "Some vector stuff", 20, 30, true, 3);
+			INSERT INTO item (item_name, item_desc, item_price, item_stock, item_img, cat_id) VALUES ("GIMP", "open source lol", 250, 10, true, 3);';
 		
 		$res = $dbh->query($sql);
 
