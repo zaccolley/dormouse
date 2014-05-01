310a40e163517b6192e29ae00a1e36b41ffc102a
Zac Colley <hi@zaccolley.com>
2014-05-01 03:50:42 +0100

    Fixed id errors with images

--- app/fileupload.php
+++ app/fileupload.php
@@ -2,9 +2,17 @@
 
 	// this file is for uploading images
 
-	$id = $_POST["id"];
+	include('data/db.php');
+
+	$sql = "SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'hedgerows' AND TABLE_NAME = 'item';";
+
+	$res = $dbh->query($sql);
+
+	$id = $res->fetch()[0];
 
 	$path = "images/".$id;
+
+	echo $path;
 
 	if(move_uploaded_file($_FILES["image"]["tmp_name"], $path)){
 		$result = true;
